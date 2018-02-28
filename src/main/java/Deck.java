243c0dd25f10d91d6d40abf6b49824cd00168aa0
Jim Bevington <jim.bevington@hotmail.com>
2018-02-28 11:53:48 +0000

    created Deck deal() method and canDealCard() test.

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Deck {
 
@@ -6,6 +7,11 @@ public class Deck {
 
     public Deck(){
         cards = new ArrayList<>();
+    }
+
+    //    just for testing
+    public ArrayList<Card> getCards() {
+        return cards;
     }
 
     public int cardCount(){
@@ -16,13 +22,18 @@ public class Deck {
 
       for (SuitType suit : SuitType.values()){
           for (ValueType value : ValueType.values()) {
+
               Card card = new Card(suit, value);
               this.cards.add(card);
           }
       }
     }
 
-    public ArrayList<Card> getCards() {
-        return cards;
+    public Card deal(){
+//        remove/return a random card from the deck
+        Collections.shuffle(this.cards);
+        return this.cards.remove(0);
     }
+
+
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,7 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class DeckTest {
 
@@ -26,7 +28,14 @@ public class DeckTest {
         assertEquals(1, deck.getCards().get(0).getValueFromEnum());
 //        check SuitType and Value of 14th card, expecting Two of Clubs
         assertEquals(SuitType.CLUBS, deck.getCards().get(14).getSuit());
-//        check Value of 14th card
         assertEquals(2, deck.getCards().get(14).getValueFromEnum());
+    }
+
+    @Test
+    public void canDealCards(){
+        deck.populate();
+        Card card = deck.deal();
+//        check that the deck no longer contains the dealt card
+        assertFalse(deck.getCards().contains(card));
     }
 }
